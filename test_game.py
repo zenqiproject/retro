f83bb25ecacad8a91f30bf39121928c7931907e5
Zenqi <zenaiwellplayed@gmail.com>
2021-04-20 18:42:19 +0800

    add test

--- test_game.py
+++ test_game.py
@@ -1,1 +1,25 @@
+from retro2d.game import Game
+from retro2d.scene import Scene
+from pygame.locals import *
 
+class GameScene(Scene):
+    def __init__(self):
+        super().__init__() # Inherit Scene class
+    
+    def ProcessInput(self, events, pressed_keys): # a function for handling inputs
+        for event in events:
+            if event.type == KEYDOWN and event.key == K_RETURN:
+                print("Enter button is pressed")
+    
+    def Update(self): # a function that updates every frame
+        pass
+    
+    def Render(self, screen): # a function that handle rendering stuff.
+        screen.fill((255, 255, 255)) # fill the background to white
+    
+    
+game = Game(
+  scene = GameScene(),
+  fullscreen = True,
+  title = "Retro 2D with pygame"
+)
